bf1d35b55df9fb9fcb927d13faddd02eab0611b2
Ben <si_ben_tran@hotmail.com>
2023-12-21 11:30:38 +0100

    restructure npr mc2

--- mini-challenge2/rasa_chatbot/actions/actions.py
+++ mini-challenge2/rasa_chatbot/actions/actions.py
@@ -1,27 +1,55 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class getProperties(Action):
+    def name(self) -> Text:
+        return "action_show_properties"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    # Figure out how to extract the property wanted by user
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker):
+        # Import data, filter on property type, return X properties then ask
+        # user for more info/details/confirmation to narrow down search in next
+        # action
+        pass
+
+
+class getPrice(Action):
+    def name(self) -> Text:
+        return "action_provide_price_info"
+
+    # Figure out how to extract the property details
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker):
+        # Import data, filter or details given, return price info on available
+        # properties then ask user for more info/details/confirmation
+        pass
+
+
+class getLocation(Action):
+    def name(self) -> Text:
+        return "action_provide_location_info"
+
+    # Figure out how to extract detials from user input
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker):
+        # Import data, filter or details given, return location info on available
+        # properties and ask for confirmation
+        # In the story it calls provide_more_details so execute that next
+        pass
+
+
+class getPropertyDetails(Action):
+    def name(self) -> Text:
+        return "action_provide_property_details"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker):
+        pass
+
+
+class getLocationDetails(Action):
+    def name(self) -> Text:
+        return "action_provide_more_details"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker):
+        pass
